0d236fde1b0be758229e2d1f2c3b9b0db5321a63
Adonis <nobimetanguy19@gmail.com>
2023-10-17 14:33:32 +0100

    Modification de callback

--- pages/champion.jsx
+++ pages/champion.jsx
@@ -37,21 +37,24 @@ export default function Champion() {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         const userId = user.uid;
-        setFormData({ ...formData, email: user.email });
-
+        setFormData((prevData) => ({
+          ...prevData,
+          email: user.email,
+        }));
+  
         try {
           const docRef = doc(db, 'users', userId);
           const docSnapshot = await getDoc(docRef);
-
+  
           if (docSnapshot.exists()) {
             const data = docSnapshot.data();
-            setFormData({
-              ...formData,
+            setFormData((prevData) => ({
+              ...prevData,
               fullName: data.fullName,
               phoneNumber: data.phoneNumber,
               profileImageUrl: data.profileImageUrl,
               champion: data.champion,
-            });
+            }));
           } else {
             console.log("Aucune donnée trouvée pour cet utilisateur.");
           }
@@ -61,8 +64,8 @@ export default function Champion() {
       } else {
         console.log("L'utilisateur n'est pas authentifié.");
       }
-    });
-  }, [formData]);
+    }, []); // Les dépendances sont maintenant un tableau vide pour exécuter le useEffect une seule fois
+  });
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -200,7 +203,7 @@ export default function Champion() {
             </select>
           </div>
 
-          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-500 dark:hover:bg-gray-800 dark:bg-gray-100 hover-bg-gray-100 dark:border-gray-600 dark:hover:border-gray-200 dark:hover:bg-gray-200">
+          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-500 dark:hover:bg-gray-800 dark:bg-gray-100 hover-bg-gray-100 dark:border-gray-600 dark:hover:border-gray-200">
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
