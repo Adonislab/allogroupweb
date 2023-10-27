37e8fd6ccfaffd7228d4106b0aae2abbb09723e5
Adonis <nobimetanguy19@gmail.com>
2023-10-27 18:20:18 +0100

    Mise en place du tableau de board

--- pages/components/partials/sidebar.jsx
+++ pages/components/partials/sidebar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 // Définir des constantes pour les rôles
 const ROLES = {
@@ -13,6 +14,7 @@ const ROLES = {
 };
 
 export default function Sidebar() {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [userRoles, setUserRoles] = useState([]);
   const [rolesWithTrueHasRole, setRolesWithTrueHasRole] = useState([]);
@@ -58,34 +60,79 @@ export default function Sidebar() {
         url: '/dashboard',
         icon: 'speedometer2',
       },
-    ],
-    [ROLES.DECIDEUR]: [
       {
-        name: 'ChampionBoard',
-        url: '/dashboard',
-        icon: 'speedometer2',
-      },  
+        name: 'Rapports',
+        url: '/repports',
+        icon: 'clipboard2-data',
+      },
       {
         name: "Compte Champion",
-        url: "/marchand",
+        url: "/champion",
         icon: "question-diamond-fill"
-     },
+      },
       {
-        name: 'Gestionnaire de Compte',
-        url: '/setting',
-        icon: 'question-diamond-fill',
-      },    
+      name: "Compte Marchand",
+      url: "/marchand",
+      icon: "question-diamond-fill"
+      },
+      {
+      name: 'Création de produits',
+      url: '/chat',
+      icon: 'question-diamond-fill',
+      },  
+      {
+        name: "Notification",
+        url: "/notification",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Gestion de l'Application",
+        url: "/gestion_appli",
+        icon: "question-diamond-fill"
+      },
     ],
-    [ROLES.COMPTABLE]: [
+    [ROLES.DECIDEUR]: [
       {
         name: 'Rapports',
         url: '/reports',
         icon: 'clipboard2-data',
       },
+      {
+        name: 'Tableau de board',
+        url: '/dashboard',
+        icon: 'speedometer2',
+      },  
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+     },
+     {
+      name: "Notification",
+      url: "/notification",
+      icon: "question-diamond-fill"
+      },
+    ],
+    [ROLES.COMPTABLE]: [
+      {
+        name: 'Tableau de board',
+        url: '/dashboard',
+        icon: 'speedometer2',
+      }, 
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+     }, 
     ],
     [ROLES.MARCHAND]: [
       {
-        name: 'Marchand Board',
+        name: 'Tableau de board',
         url: '/dashboard',
         icon: 'speedometer2',
       },  
@@ -98,12 +145,7 @@ export default function Sidebar() {
         name: 'Création de produits',
         url: '/chat',
         icon: 'question-diamond-fill',
-      },
-      {
-        name: 'Gestionnaire de Compte',
-        url: '/setting',
-        icon: 'question-diamond-fill',
-      },    
+      },  
       {
         name: "Compte utilisateur",
         url: "/users",
@@ -112,25 +154,25 @@ export default function Sidebar() {
     ],
     [ROLES.CHAMPION]: [
       {
-        name: 'Champion Board',
+        name: 'Tableau de board',
         url: '/dashboard',
         icon: 'speedometer2',
       },  
       {
         name: "Compte Champion",
-        url: "/marchand",
+        url: "/champion",
         icon: "question-diamond-fill"
      },
-      {
-        name: 'Gestionnaire de Compte',
-        url: '/setting',
-        icon: 'question-diamond-fill',
-      },    
       {
         name: "Compte utilisateur",
         url: "/users",
         icon: "question-diamond-fill"
      },
+     {
+      name: "Notification",
+      url: "/notification",
+      icon: "question-diamond-fill"
+    },
     ]
   };
 
@@ -156,7 +198,7 @@ export default function Sidebar() {
                   if (roleItem) {
                     return roleItem.map((item, index) => (
                       <li key={index}>
-                        <Link href={item.url} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover-bg-gray-700 group">
+                        <Link href={item.url} className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover-bg-gray-700 group ${router.asPath === item.url ? 'bg-orange-500' : ''}`}>
                           <i className={"bg-blue-900 p-1 px-2 rounded-md bi bi-" + item.icon}></i>
                           <span className="ml-3 font-semibold">{item.name}</span>
                         </Link>
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,166 +1,118 @@
 import DashLayout from "./components/layout/dashboardLayout";
 import Head from "@/utils/head";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { firebaseConfig } from '../utils/firebaseConfig';
 
 function Dashboard() {
-    return (
-      
-        <DashLayout>
-            <Head/>
-            <div className="p-4 border- border-gray-20 border-dashe rounded-lg dark:border-gray-700 mt-14">
-                <div className="grid grid-cols-3 gap-4 mb-4">
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="mt- flex p-4">
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-                <form  onSubmit={"/"} className="flex items-center justify-center w-full">
-                    <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-500 dark:hover:bg-bray-800 dark:bg-gray-100 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-200 dark:hover:bg-gray-200">
-                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                            </svg>
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                        </div>
-                        <input id="dropzone-file" type="file" className="hidden" />
-                        
-                            <div className="w-3/4 mx-auto bg-gray-200 rounded-full dark:bg-gray-600">
-                                <div className="bg-orange-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: "45%"}}> 45%</div>
-                            </div>
-                            <div className="mt-8">
+  
+  const COLUMNS = [
+    { label: <span className="text-blue-500">Utilisateur</span>, renderCell: (item) => item.fullName },
+    {
+      label: <span className="text-blue-500">Type de profil</span>,
+      renderCell: (item) => (item.admin),
+    },
+    { label: <span className="text-blue-500">Téléphone</span>, renderCell: (item) => item.phoneNumber },
+    { label: <span className="text-blue-500"></span>, renderCell: (item) => (
+        <button className=" text-white bg-purple-500 hover:text-white focus:outline-none" onClick={() => handleEdit(item)}>Modifiez</button>)
+    },
+    { label: <span className="text-blue-500"></span>, renderCell: (item) => (
+        <button className=" bg-red-500 text-white hover:text-white focus:outline-none" onClick={() => handleDelete(item)}>Supprimez</button>) 
+        },
+    ];
 
-                            <button type="submit" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Process</button>
-                            </div>
-                            {/* <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Light</button> */}
+    const handleEdit = (item) => {
+        alert('Modification');
+    };
 
-                    </label>
-                </form>
+    const handleDelete = async (item) => {
+        if (window.confirm("Voulez-vous vraiment supprimer cet utilisateur ?")) {
+         
+          const auth = getAuth();
+          const db = getFirestore(firebaseConfig);
+          const userId = item.id; 
+          try {
+            await deleteDoc(doc(db, "users", userId));
+            // Réactualisez les données après la suppression.
+            const usersRef = collection(db, "users");
+            const querySnapshot = await getDocs(usersRef);
+            const userDataArray = [];
+            querySnapshot.forEach((doc) => {
+              userDataArray.push({ id: doc.id, ...doc.data() });
+            });
+            setUserData(userDataArray);
+          } catch (error) {
+            console.error("Erreur lors de la suppression de l'utilisateur", error);
+          }
+        }       
+    }
 
-            </div>
+  useEffect(() => {
+    const auth = getAuth();
+    const db = getFirestore(firebaseConfig);
 
-            {/* <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                <div className="grid grid-cols-3 gap-4 mb-4">
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                    </p>
-                </div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                    </p>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
-                        </p>
-                    </div>
-                </div>
-            </div> */}
-            
-            
-            </DashLayout>
-    );
+    onAuthStateChanged(auth, async (authUser) => {
+      if (authUser) {
+        setUser(authUser);
+
+        const usersRef = collection(db, "users");
+        const querySnapshot = await getDocs(usersRef);
+
+        const userDataArray = [];
+        querySnapshot.forEach((doc) => {
+          userDataArray.push(doc.data());
+        });
+
+        setUserData(userDataArray);
+        setLoading(false);
+      } else {
+        setUser(null);
+        setUserData([]);
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return <p>Chargement...</p>;
+  }
+
+  return (
+    <DashLayout>
+      <Head/>
+      <div className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-14">
+        <table className="w-full table-fixed">
+            <thead>
+            <tr>
+                {COLUMNS.map((column, index) => (
+                <th key={index} className="px-4 py-2">{column.label}</th>
+                ))}
+            </tr>
+            </thead>
+            <tbody>
+            {userData.map((item, index) => (
+                <tr key={index}>
+                {COLUMNS.map((column, columnIndex) => (
+                    <td key={columnIndex} className="px-4 py-2 border">
+                    {column.renderCell(item)}
+                    </td>
+                ))}
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        
+      </div>
+      <div>
+        <p className="mt-4 text-2xl text-orange-500"> Quelles sont les grandes tendances Allô Group ?</p>
+      </div>
+   </DashLayout>
+  );
 }
 
 export default Dashboard;
