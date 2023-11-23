0fbe515fa72b63d385a9bb27149d6fec8b02de94
Adonis <nobimetanguy19@gmail.com>
2023-11-23 11:44:05 +0100

    Modification pour id livreur et demande de photo identité

--- pages/champion.jsx
+++ pages/champion.jsx
@@ -91,6 +91,7 @@ export default function Champion() {
   
         // Utilisez l'URL de téléchargement dans le champ avatar
         await setDoc(docRef, {
+          id:userId,
           fullName: fullName,
           phoneNumber: phoneNumber,
           profileImageUrl: newImageURL,
@@ -201,7 +202,7 @@ export default function Champion() {
               <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
               </svg>
-              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Téléversez votre photo</span> ou glissez et déposez</p>
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Téléversez votre photo d'identité</span> ou glissez et déposez</p>
               <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
             </div>
             <input id="dropzone-file" type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange} />
