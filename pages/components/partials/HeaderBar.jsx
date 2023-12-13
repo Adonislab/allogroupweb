769078d880f897ad8857dad48f6060f7a9409ee5
Adonis <nobimetanguy19@gmail.com>
2023-12-13 16:03:26 +0100

    Résolution du soucis de gestion mobile de la barre de navigation

--- pages/components/partials/HeaderBar.jsx
+++ pages/components/partials/HeaderBar.jsx
@@ -2,17 +2,26 @@ import Image from "next/image";
 import Logo from "../../../utils/Assets/Logo.png";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import Link from "next/link";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+
+// Définir des constantes pour les rôles
+const ROLES = {
+  ADMIN: 'admin',
+  COMPTABLE: 'comptable',
+  DECIDEUR: 'decideur',
+  CHAMPION: 'champion',
+  MARCHAND: 'marchand',
+};
+
 
 function HeaderBar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const router = useRouter();
-  const [formData, setFormData] = useState({
-    phoneNumber: "",
-    fullName: "",
-  });
-
+  const [user, setUser] = useState(null);
+  const [userRoles, setUserRoles] = useState([]);
+  
   const handleMenuClick = () => {
     setMenuOpen(!menuOpen);
   };
@@ -24,34 +33,183 @@ function HeaderBar() {
 
   useEffect(() => {
     const auth = getAuth();
-    const fetchData = async (user) => {
-      if (user) {
-        const userId = user.uid;
-        try {
-          const docRef = doc(db, 'users', userId);
-          const docSnapshot = await getDoc(docRef);
-    
-          if (docSnapshot.exists()) {
-            const data = docSnapshot.data();
-            setFormData((prevData) => ({
-              ...prevData,
-              fullName: data.fullName,
-              phoneNumber: data.phoneNumber,
-            }));
-          } else {
-            console.log("Aucune donnée trouvée pour cet utilisateur.");
-          }
-        } catch (error) {
-          console.error('Erreur lors de la récupération des données depuis Firestore', error);
-        }
+    onAuthStateChanged(auth, async (authUser) => {
+      if (authUser) {
+        setUser(authUser);
+
+        const db = getFirestore();
+        // Utilisez authUser.uid pour obtenir l'ID de l'utilisateur connecté
+        const userId = authUser.uid;
+        
+        const userRolesDoc = doc(db, 'users', userId);
+
+        // Exécutez la requête pour récupérer les rôles de l'utilisateur
+        getDoc(userRolesDoc)
+          .then((docSnapshot) => {
+            if (docSnapshot.exists()) {
+              const data = docSnapshot.data();
+              const rolesWithTrueHasRole = Object.keys(data).filter((role) => data[role] === true);
+              setUserRoles(rolesWithTrueHasRole);
+            }
+          })
+          .catch((error) => {
+            console.error('Erreur lors de la récupération des rôles:', error);
+          });
       } else {
-        console.log("L'utilisateur n'est pas authentifié.");
+        setUser(null);
+        // Ne pas définir userRoles à vide pour afficher la page "Accueil" par défaut
       }
-    };
-    
-    onAuthStateChanged(auth, fetchData);
+    });
   }, []);
 
+   // Exemple de structure d'éléments liés aux rôles
+   const roleItems = {
+    [ROLES.ADMIN]: [
+      {
+        name: 'Tableau Administrateur',
+        url: '/dashboard',
+        icon: 'speedometer2',
+      },
+      {
+        name: "Liste des marchands",
+        url: "/listeMarchand",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Liste des champions",
+        url: "/listeChampion",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Vues des décideurs",
+        url: "/listeDecideur",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Vue des comptables",
+        url: "/listeComptable",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Gestion des rôles",
+        url: "/gestionRole",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: 'Rapports',
+        url: '/repports',
+        icon: 'clipboard2-data',
+      },
+      {
+        name: "Gestion des notifications",
+        url: "/notification",
+        icon: "question-diamond-fill"
+      },
+      
+      {
+        name: "Gestion de l'Application",
+        url: "/gestion_appli",
+        icon: "question-diamond-fill"
+      },
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+      },
+    ],
+    [ROLES.DECIDEUR]: [
+      {
+        name: 'Rapports',
+        url: '/reports',
+        icon: 'clipboard2-data',
+      },
+      {
+        name: 'Tableau de décison',
+        url: '/dashboard',
+        icon: 'speedometer2',
+      },  
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+     },
+     {
+      name: "Systeme de notification",
+      url: "/notification",
+      icon: "question-diamond-fill"
+      },
+    ],
+    [ROLES.COMPTABLE]: [
+      {
+        name: 'Tableau de Vison',
+        url: '/dashboard',
+        icon: 'speedometer2',
+      }, 
+      {
+        name: "Compte utilisateur",
+        url: "/users",
+        icon: "question-diamond-fill"
+     }, 
+    ],
+    [ROLES.MARCHAND]: [
+      {
+        name: 'Mes ventes',
+        url: '/venteMarchand',
+        icon: 'speedometer2',
+      },  
+      {
+        name: 'Marchand Board',
+        url: '/dashboardMarchand',
+        icon: 'speedometer2',
+      }, 
+      {
+        name: "Notification de vente",
+        url: "/notification",
+        icon: "question-diamond-fill"
+      }, 
+      {
+        name: "Compte Marchand",
+        url: "/marchand",
+        icon: "question-diamond-fill"
+     },
+      {
+        name: 'Création de produits',
+        url: '/chat',
+        icon: 'question-diamond-fill',
+      },  
+    ],
+    [ROLES.CHAMPION]: [
+      {
+        name: 'Champion Board',
+        url: '/dashboardChampion',
+        icon: 'speedometer2',
+      },  
+      {
+        name: "Compte Champion",
+        url: "/champion",
+        icon: "question-diamond-fill"
+     },
+     {
+      name: "Notification de livraison",
+      url: "/notification",
+      icon: "question-diamond-fill"
+    },
+    ]
+  };
+
+  // Élément de menu générique pour l'utilisateur sans rôle
+  const defaultMenuItem = [
+    {
+      name: 'Accueil',
+      url: '/politique',
+      icon: 'question-diamond-fill',
+    },
+    {
+      name: 'Partenariat',
+      url: '/setting',
+      icon: 'question-diamond-fill',
+    },
+  ];
 
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -86,48 +244,34 @@ function HeaderBar() {
                 menuOpen ? "block" : "hidden"
               } absolute w-40 bg-white rounded-md border border-gray-200 right-0 mt-12 z-10`}
             >
-              <ul className="text-gray-900">
-                <li>
-                  <button
-                    onClick={() => handleLinkClick("/")}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100"
-                  >
-                    Home
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => handleLinkClick("/dashboard")}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100"
-                  >
-                    Dashboard
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => handleLinkClick("/setting")}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100"
-                  >
-                    Settings
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => handleLinkClick("/earnings")}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100"
-                  >
-                    Earnings
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => handleLinkClick("/sign-out")}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100"
-                  >
-                    Sign Out
-                  </button>
-                </li>
-              </ul>
+             <ul className="space-y-2 font-medium">
+          {userRoles.length > 0 ? (
+              userRoles.map((role) => {
+                const roleItem = roleItems[role];
+                if (roleItem) {
+                  return roleItem.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover-bg-gray-700 group ${router.asPath === item.url ? 'bg-orange-500' : ''}`}>
+                        <i className={"bg-blue-900 p-1 px-2 rounded-md bi bi-" + item.icon}></i>
+                        <span className="ml-3 font-semibold">{item.name}</span>
+                      </Link>
+                    </li>
+                  ));
+                }
+                return null; // Rôle non trouvé dans roleItems
+            })
+          ) : (
+            // Afficher la page "Accueil" si l'utilisateur n'a pas de rôle ou si aucun rôle avec `hasRole` à `true` n'a été trouvé
+            defaultMenuItem.map((item, index) => (
+              <li key={index}>
+                <Link href={item.url} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover-bg-gray-700 group">
+                  <i className={"bg-blue-900 p-1 px-2 rounded-md bi bi-" + item.icon}></i>
+                  <span className="ml-3 font-semibold">{item.name}</span>
+                </Link>
+              </li>
+            ))
+          )}
+        </ul>
             </div>
             <Image
               src={Logo}
