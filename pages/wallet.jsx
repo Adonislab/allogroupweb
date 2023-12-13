84f7cee5aea502669b791543c626cd9e2c0262cb
Adonis <nobimetanguy19@gmail.com>
2023-12-13 16:31:52 +0100

    Mise en place des pages selon les droits d'auteurs

---
+++ pages/wallet.jsx
@@ -0,0 +1,15 @@
+import DashLayout from "./components/layout/dashboardLayout";
+import Head from "@/utils/head";
+
+function wallet() {
+  return (
+    <DashLayout>
+    <Head/>
+      <div className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-14">
+        wallet
+      </div>
+    </DashLayout> 
+  )
+}
+
+export default wallet
