ff906e51ee316115e1dfc04ad639aa0d45e7ddf7
Adonis <nobimetanguy19@gmail.com>
2023-12-15 12:49:28 +0100

    Modification pour intégration de Kikipay mais souci la valeur

--- pages/paiementChampion.jsx
+++ pages/paiementChampion.jsx
@@ -1,20 +1,65 @@
+'use client'
+import { useKKiaPay } from 'kkiapay-react';
 import DashLayout from "./components/layout/dashboardLayout";
 import Head from "@/utils/head";
 import { firebaseConfig } from '../utils/firebaseConfig';
-import React, { useState, useEffect, Input} from 'react';
+import React, { useState, useEffect} from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import {  doc, getDoc,getFirestore } from 'firebase/firestore';
-
-
+import {  doc, getDoc,getFirestore, setDoc } from 'firebase/firestore';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Router from 'next/router';
+ 
 function paiementChampion() {
-
+  const { openKkiapayWidget, 
+          addKkiapayListener,
+          removeKkiapayListener 
+        } = useKKiaPay();
   const db = getFirestore(firebaseConfig);
+  const auth = getAuth();
   const [formData, setFormData] = useState({
     phoneNumber: "",
     wallet:"",
+    credit:"",
   });
-
+ 
+  
+   function successHandler(response) {
+    console.log("reussite");
+    console.log(response);
+    const { credit } = formData;
+    const userId = auth.currentUser.uid;
+    const docRef = doc(db, 'users', userId);
+    const actuel = docRef['wallet'] + credit;
+    setDoc(
+      docRef,
+      {
+        wallet: actuel,
+      },
+      { merge: true }
+    )
+      .then(() => {
+        toast.success(`Vous avez crédité votre compte de ${credit} F`);
+        setTimeout(() => {
+          Router.push("/wallet");
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la mise à jour du compte utilisateur', error);
+        toast.error('Erreur lors de la mise à jour du compte utilisateur');
+      });
+  };
+  
+  function failureHandler(error) {
+    console.log('Echec');
+    console.log(error);
+    toast.error("Votre paiement au service est un échec");
+  }
+  
+   
   useEffect(() => {
+    addKkiapayListener('success', successHandler)
+    addKkiapayListener('failed', failureHandler)
     const auth = getAuth();
     const fetchData = async (user) => {
       if (user) {
@@ -44,7 +89,11 @@ function paiementChampion() {
     };
     
     onAuthStateChanged(auth, fetchData);
-  }, []);
+    return () => {
+      removeKkiapayListener('success',successHandler);
+      removeKkiapayListener('failed', failureHandler);
+    }
+  }, [addKkiapayListener,removeKkiapayListener]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -54,12 +103,19 @@ function paiementChampion() {
     });
   };
 
+  function open(credit) {
+    openKkiapayWidget({
+      amount: credit,
+      api_key: "3425dc6035d711eca8f5b92f2997955b",
+      sandbox: true,
+      email: "randomgail@gmail.com",
+      phone: "97000000",
+    });
+  }
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    open(formData.credit);
   };
-
-
 
   return (
     <DashLayout>
@@ -74,7 +130,6 @@ function paiementChampion() {
               name="phoneNumber"
               required={true}
               value={formData.phoneNumber}
-              onChange={handleInputChange}
               className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-1"
 
             />
@@ -88,15 +143,28 @@ function paiementChampion() {
               name="wallet"
               required={true}
               value={formData.wallet}
+              className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-1"
+            />  <span className="text-white">FCFA</span>
+          </div> 
+          <div className='my-5'>
+          <p className="text-white"> Votre paiement </p>
+
+            <input
+              type='text'
+              label="Montant"
+              name="credit"
+              required={true}
+              value={formData.credit}
               onChange={handleInputChange}
               className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-1"
             />  <span className="text-white">FCFA</span>
           </div> 
           
-          <input type="submit" value="Paiement d'un service" className='bg-white montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer text-orange-500' />
+          <input type="submit" value="Paiement du service" className='bg-white montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer text-orange-500' />
           
       </form>
     </div>
+    <ToastContainer />
     </DashLayout> 
   )
 }
