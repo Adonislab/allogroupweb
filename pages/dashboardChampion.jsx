5ea7910f1ed0e4309e9bf5569fe261c1863a8e7a
Moubarak321 <benmoumass@gmail.com>
2023-12-22 19:21:03 +0100

    ajout de la pagination dashboardChampion

--- pages/dashboardChampion.jsx
+++ pages/dashboardChampion.jsx
@@ -5,6 +5,8 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, getDoc} from 'firebase/firestore';
 import Link from 'next/link';
 import { firebaseConfig } from '../utils/firebaseConfig';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -12,8 +14,51 @@ function DashboardChampion() {
   const [user, setUser] = useState(null);
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [livraisonsPerPage] = useState(5);
+  
+
+
+
+  const formatDateTime = (timestamp) => {
+    if (!timestamp || isNaN(timestamp)) {
+      return ''; // ou une valeur par défaut si timestamp est undefined, null ou non un nombre
+    }
+  
+    const date = new Date(Number(timestamp));
+    const options = {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second:'numeric'
+    };
+  
+    return new Intl.DateTimeFormat('fr-FR', options).format(date);
+  };
   
   
+    const deliveryInfos = (item) => {
+      alert(`
+        Commanditaire: ${item.numeroARecuperation}
+        Adresse de recupération du colis: ${item.addressRecuperation}
+        Destinataire: ${item.numeroALivraison}
+        Adresse de destination du colis: ${item.addressLivraison}
+        Colis: ${item.title}
+        Type de course: ${item.type_courses}
+        prix: ${item.prix}
+        Date de Livraison: ${formatDateTime(item.dateDeLivraison)}
+      `);
+    };
+
+
+
+
+
+
+
+
   const COLUMNS = [
     { label: <span className="text-blue-500">Quoi</span>, renderCell: (item) => item.title },
     {
@@ -29,7 +74,7 @@ function DashboardChampion() {
       ),
     },
     { label: <span className="text-blue-500">Détails</span>, renderCell: (item) => (
-        <button className="bg-blue-500 text-white hover:text-white focus:outline-none">Plus</button>)
+        <button className="bg-blue-500 text-white hover:text-white focus:outline-none" onClick={() => deliveryInfos(item)}><FontAwesomeIcon icon={faInfoCircle} /></button>)
     },
   ];
 
@@ -74,6 +119,14 @@ function DashboardChampion() {
       }
     });
   }, []);
+  // Pagination
+  const indexOfLastLivraison = currentPage * livraisonsPerPage;
+  const indexOfFirstLivraison = indexOfLastLivraison - livraisonsPerPage;
+  const currentLivraisons = products.slice(indexOfFirstLivraison, indexOfLastLivraison);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const nextPage = () => setCurrentPage(currentPage + 1);
+  const prevPage = () => setCurrentPage(currentPage - 1);
 
   if (loading) {
     return <p>Chargement...</p>;
@@ -84,18 +137,20 @@ function DashboardChampion() {
       <Head />
       
       <div className="p-4 border border-gray-20 border-dashe rounded-lg dark:border-orange-500 mt-14">
-        <p className="mt-4 text-2xl text-orange-500">Quelles sont les grandes tendances de livraion Allô Group ?</p>
+        {/* <p className="mt-4 text-2xl text-orange-500">Quelles sont les grandes tendances de livraion Allô Group ?</p> */}
         <p className="text-2xl text-blue-500">Nombre de livraison effectué : {products.length}</p>
         <table className="w-full table-fixed">
           <thead>
-            <tr>
+          <tr>
               {COLUMNS.map((column, index) => (
-                <th key={index} className="px-4 py-2">{column.label}</th>
+                <th key={index} className="px-4 py-2">
+                  {column.label}
+                </th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {products.map((item, index) => (
+            {currentLivraisons.map((item, index) => (
               <tr key={index}>
                 {COLUMNS.map((column, columnIndex) => (
                   <td key={columnIndex} className="px-4 py-2 border">
@@ -106,8 +161,43 @@ function DashboardChampion() {
             ))}
           </tbody>
         </table>
+
+       
+
+
       </div>
-      
+       {/* Pagination */}
+       <div className="mt-4 flex justify-between">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="bg-blue-500 text-white px-4 py-2 rounded-full"
+        >
+          Précédent
+        </button>
+        <div className="flex space-x-2">
+          {Array.from({ length: Math.ceil(products.length / livraisonsPerPage) }).map((_, index) => (
+            <div
+              key={index}
+              onClick={() => paginate(index + 1)}
+              className={`cursor-pointer hover:underline px-2 py-2 ${
+                currentPage === index + 1 ? 'bg-blue-500 text-white rounded-full' : ''
+              }`}
+            >
+              {index + 1}
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === Math.ceil(products.length / livraisonsPerPage)}
+          className="bg-blue-500 text-white px-4 py-2 rounded-full"
+        >
+          Suivant
+        </button>
+      </div>
+      {/* End Pagination */}
+
     </DashLayout>
   );
 }
