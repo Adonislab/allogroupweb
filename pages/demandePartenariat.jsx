c44994d7df68a735b1b15df6704a49831b1bf276
Adonis <nobimetanguy19@gmail.com>
2024-01-03 18:40:23 +0100

    Fixation du bug de demannde de partenariat

--- pages/demandePartenariat.jsx
+++ pages/demandePartenariat.jsx
@@ -43,7 +43,10 @@ export default function Setting() {
         const downloadURL = await getDownloadURL(snapshot.ref);
         return downloadURL;
     };
-  
+    const handleStatusChange = (e) => {
+        // Mettre à jour le statut sélectionné lorsque l'utilisateur change la liste déroulante
+        setFormData({ ...formData, selectedStatus: e.target.value });
+    };
 
     useEffect(() => {
         const auth = getAuth();
@@ -198,9 +201,39 @@ export default function Setting() {
                         />
                     </div>
 
+                    <div className='text-left'>        
+                        <fieldset>
+                            <legend>Les types  de partenariat sont :</legend>
+
+                            <div>
+                                <input type="radio"/>
+                                <label for="huey">Marchand (Gestionnaire de Fast Food)</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" />
+                                <label for="dewey">Livreur (Activité de livraison de bien)</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" />
+                                <label for="louie">Chauffeur (Activité de conduite de personne)</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" />
+                                <label for="louie">Shopkeeper (Gestionnaire de boutique de vente de bien)</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" />
+                                <label for="louie">Gestionnaire d'évènement (Gestionnaire de boutique d'évenement comme les établissements de décoration)</label>
+                            </div>
+                        </fieldset>
+                    </div>    
 
                     <div className='text-left'>
-                        <label htmlFor="categorie" className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">
+                        <label >
                             Choisissez le type de partenariat
                         </label>
 
@@ -221,7 +254,6 @@ export default function Setting() {
 
                         </select>
                     </div>
-                    
                     <label htmlFor="idCard" className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Chargez votre Chargez votre Pièce d'identité(CIP,CIN,Passeport,Carte biométrique)</label>
 
                     <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer  dark:hover:bg-gray-800 dark:bg-gray-100 hover-bg-gray-100 dark:border-gray-600 dark:hover:border-gray-200">
@@ -241,6 +273,30 @@ export default function Setting() {
                             </div>
                         ) : null}
                     </label>
+
+
+
+                    {/* <label htmlFor="idCard" className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Chargez votre Pièce d'identité(CIP,CIN,Passeport,Carte biométrique)</label>
+
+                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer  dark:hover:bg-gray-800 dark:bg-gray-100 hover-bg-gray-100 dark:border-gray-600 dark:hover:border-gray-200">
+                        Chargez votre Pièce d'identité
+                        <div className="flex flex-col items-center justify-center pt-3 pb-4">
+                            <svg className="w-8 h-8 mb-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                            </svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Téléversez votre document</span> ou glissez et déposez</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400">JPG,PNG,JPEG (MAX. 5mo)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange} />
+
+                        {formData.selectedFile ? (
+                            <div className="w-3/4 mx-auto bg-gray-200 rounded-full dark:bg-gray-600">
+                                <div className="bg-orange-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: "100%" }}> 100%</div>
+                            </div>
+                        ) : null}
+                    </label> */}
+
+
 
                     <button type="submit" className="w-full text-white bg-indigo-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Soumettre</button>
                 </form>
